75701e1bec271333d72299d72a52c911a8ac68dc
yoj9168@gmail.com <yoj9168@gmail.com>
2023-02-22 14:15:05 +0900

    first commit

---
+++ src/test/java/com/word/game/word_game/WordGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word.game.word_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
